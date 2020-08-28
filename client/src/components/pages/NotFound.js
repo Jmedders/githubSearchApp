994cd0a68511f19e81ea9ae534827e01b9b46deb
Jay <medders.jeffrey@gmail.com>
2020-08-27 23:57:59 -0600

    add not found page, minimal style

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
-  return <></>;
+  return (
+    <>
+      There's nothing here, head <StyledLink to="/">home</StyledLink>
+    </>
+  );
 }
+
+const StyledLink = styled(Link)`
+  color: #678cd9;
+`;
