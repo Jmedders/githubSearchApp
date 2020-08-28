15d26db76ac07749982e7f8719fa2a511b6fc017
Jay <medders.jeffrey@gmail.com>
2020-08-28 00:26:55 -0600

    initial styling of the splash page

--- client/src/components/pages/Splash.js
+++ client/src/components/pages/Splash.js
@@ -1,15 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
 import InputSearch from "../Search/InputSearch";
 import Filter from "../Search/Filter";
 import Sort from "../Search/Sort";
-import { useSearch } from "../../context/search-context";
+import Margin from "../common/Margin";
 
 export default function Splash() {
   return (
-    <>
-      <InputSearch />
-      <Filter />
-      <Sort />
-    </>
+    <Wrap>
+      <h1>Github Repository Search</h1>
+      <div>Find a Github repository by searching below.</div>
+      <Margin spaceBottom="1.5rem">
+        <InputSearch />
+      </Margin>
+      <SortWrap>
+        <Sort />
+        <Filter />
+      </SortWrap>
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  padding: 1rem 3rem;
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 800px) {
+    padding: 1rem;
+  }
+`;
+
+const SortWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 2rem;
+  max-width: 28rem;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`;
