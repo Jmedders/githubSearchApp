e5a8f73081f64eda5f1b2535d3a139bf08d61594
Jay <medders.jeffrey@gmail.com>
2020-08-28 09:01:08 -0600

    styling overhaul of listview and detailview

--- client/src/components/common/Card.js
+++ client/src/components/common/Card.js
@@ -1,6 +1,97 @@
 import React from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import Margin from "./Margin";
 
 export default function Card({ data, idx }) {
-  console.log(data);
-  return <>Card</>;
+  let history = useHistory();
+  const handleLink = (url) => {
+    window.open(url, "_blank");
+  };
+
+  const handleClick = ({ target }) => {
+    // if the element clicked on doesn't link elsewhere
+    if (target.className !== "hover") {
+      // push the user to the detail screen
+      history.push(`/view/${data.id}`, data);
+    }
+  };
+
+  return (
+    <Wrap idx={idx} onClick={handleClick}>
+      <LeftWrap className="column">
+        <RowWrap>
+          <span>
+            <strong>Name: </strong>
+            {data.name}
+          </span>
+          <Margin spaceLeft="1rem">
+            <img
+              className="hover"
+              src="/github.png"
+              style={{ height: "1rem" }}
+              onClick={() => handleLink(data.html_url)}
+              alt="link to github repository"
+            />
+          </Margin>
+        </RowWrap>
+        <div>&#9733; {data.stargazers_count}</div>
+        <div>{data.language}</div>
+      </LeftWrap>
+      <RightWrap className="column">
+        <strong>Creator:</strong>
+        <div className="hover" onClick={() => handleLink(data.owner.html_url)}>
+          {data.owner.login}
+        </div>
+        <StyledAvatar
+          className="hover"
+          onClick={() => handleLink(data.owner.html_url)}
+          src={data.owner.avatar_url}
+        />
+      </RightWrap>
+    </Wrap>
+  );
 }
+
+const Wrap = styled.div`
+  height: 5rem;
+  border: 1px solid #d9d9d9;
+  padding: 1rem;
+  flex-direction: row;
+  justify-content: space-between;
+  display: flex;
+  &:nth-child(even) {
+    background: #d9d9d9;
+  }
+  /* passing props for the fun of it */
+  border-top-left-radius: ${({ idx }) => idx === 0 && "1rem"};
+  border-top-right-radius: ${({ idx }) => idx === 0 && "1rem"};
+  /* the following is cleaner of course */
+  &:last-child {
+    border-bottom-left-radius: 1rem;
+    border-bottom-right-radius: 1rem;
+  }
+`;
+
+const RowWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const StyledAvatar = styled.img`
+  height: 2rem;
+  width: 2rem;
+  border-radius: 50%;
+`;
+
+const LeftWrap = styled.div`
+  justify-content: space-between;
+  width: 50%;
+`;
+
+const RightWrap = styled.div`
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 25%;
+`;
--- client/src/components/pages/DetailRepository.js
+++ client/src/components/pages/DetailRepository.js
@@ -1,5 +1,119 @@
 import React from "react";
+import styled from "styled-components";
+import { useLocation, Redirect, useHistory } from "react-router-dom";
+import dayjs from "dayjs";
+import Margin from "../common/Margin";
 
 export default function DetailRepository() {
-  return <></>;
+  const location = useLocation();
+  const history = useHistory();
+  const { state } = location;
+  // this data has likely never been fetched
+  // no info so push user back to root
+  if (!state) return <Redirect to="/" />;
+  // there is some state, present detail screen
+  const goToRepo = () => {
+    window.open(state.html_url, "_blank");
+  };
+  const goBack = () => {
+    history.goBack();
+  };
+  return (
+    <Wrap>
+      <h1>{state.name}</h1>
+      <Margin spaceTop="4rem">
+        <InfoWrap className="row">
+          <div>
+            <Img src={state.owner.avatar_url} />
+            <Margin spaceTop="0.4rem">
+              <h3>
+                Creator:{" "}
+                <StyledLink href={state.owner.html_url}>
+                  {state.owner.login}
+                </StyledLink>
+              </h3>
+            </Margin>
+          </div>
+          <Description>
+            <h3>Description:</h3> {state.description}
+          </Description>
+          <Meta>
+            <h3>Info:</h3>
+            <div>Stars: {state.stargazers_count}</div>
+            <div>Issues: {state.open_issues_count}</div>
+            <div>Forks: {state.forks_count}</div>
+
+            <div>Primarily written in: {state.language}</div>
+            <div>Created: {dayjs(state.created_at).format("MM/DD/YYYY")}</div>
+            <div>
+              Last updated: {dayjs(state.updated_at).format("MM/DD/YYYY")}
+            </div>
+          </Meta>
+        </InfoWrap>
+      </Margin>
+      <ButtonWrap className="row">
+        <Button left onClick={goBack}>
+          Go back
+        </Button>
+        <Button onClick={goToRepo}>Take me there!</Button>
+      </ButtonWrap>
+    </Wrap>
+  );
 }
+
+const Wrap = styled.div`
+  margin: 3rem;
+`;
+
+const StyledLink = styled.a`
+  color: rgba(103, 140, 217, 0.9);
+  &:hover {
+    cursor: pointer;
+    color: rgba(103, 140, 217, 1);
+  }
+  text-decoration: none;
+`;
+const InfoWrap = styled.div`
+  /*height: 20rem;
+  max-width: 15rem;
+	/*width: 15re*/
+  /*width: 15rem;*/
+  /*border: 0.063rem solid #9b9b9b;  */
+  justify-content: space-between;
+`;
+
+const Img = styled.img`
+  height: 10rem;
+`;
+
+const Description = styled.div`
+  width: 40%;
+`;
+
+const Meta = styled.div`
+  width: 20%;
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(103, 140, 217, 1);
+  border-radius: 0.25rem;
+  width: 9rem;
+  padding: 0.75rem;
+  color: #ffffff;
+  font-size: 0.825rem;
+  font-weight: bold;
+  &:hover {
+    cursor: pointer;
+  }
+  margin-left: ${({ left }) => !left && "1rem"};
+  margin-right: ${({ left }) => !left && "-1rem"};
+`;
+
+const ButtonWrap = styled.div`
+  margin-top: 3rem;
+  justify-content: center;
+  align-items: center;
+`;
